aeacdcc51f7d9afcbdc6899ca8fbfebcc857c92a
yullayyyy <jonashlor19@gmail.com>
2023-09-20 04:07:35 +0000

    rworksheet

---
+++ RWorksheet_Defensor.R
@@ -0,0 +1,42 @@
+#1 
+ageVector <- c(34, 28, 22, 36, 27, 18, 52, 38, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42,53, 41, 51, 35, 24, 33, 41)
+ 
+length(ageVector)
+ 
+#2 Find the reciprocal of the values for age.
+reciprocalAge <-1 /ageVector
+reciprocalAge
+ 
+#3 Assign also new_age <-c(age,0,age)
+new_ageVector <-c(ageVector,0,ageVector)
+new_ageVector
+
+#4 Sort the values for age.
+sortedAgeVector <- sort(ageVector)
+sortedAgeVector
+
+#5 Find the minimum and maximum value of age.
+minimum_ageVector <- min(ageVector)
+minimum_ageVector
+
+maximum_ageVector <- max(ageVector)
+maximum_ageVector
+
+#6 
+#b 
+new_dataVector <-c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
+#a
+length(new_dataVector)
+
+#7
+double_new_dataVector <- (new_dataVector*2)
+double_new_dataVector
+
+#8.1
+seq(1, 100, by = 1)
+
+#8.2
+seq(20,60, by = 1)
+
+#8.3
+)
